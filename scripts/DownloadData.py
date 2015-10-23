10e7938f2ea25ddd751ebc718f843557db3a0702
Scott Davis <stdavis@utah.gov>
2015-10-23 11:46:02 -0600

    better place for sde connection files

--- scripts/DownloadData.py
+++ scripts/DownloadData.py
@@ -1,6 +1,5 @@
 import arcpy
-from os import path, makedirs, listdir, unlink
-from glob import glob
+from os import path, makedirs, listdir
 from zipfile import ZipFile, ZIP_DEFLATED
 
 '''
@@ -18,7 +17,8 @@ area = arcpy.GetParameterAsText(2)
 if area == '#' or not area:
     arcpy.AddMessage('area: None')
     area = None
-reportsFC = r'C:\PythonScripts\DatabaseConnections\Roadkill_PROD as RKAdmin.sde\RoadKill.RKADMIN.Reports'
+reportsFC = path.join(path.dirname(path.abspath(__file__)),
+                      r'database_connections\Roadkill_PROD as RKAdmin.sde\RoadKill.RKADMIN.Reports')
 reports = 'reports'
 outputFolder = arcpy.env.scratchWorkspace
 if not outputFolder:
@@ -38,41 +38,41 @@ def removePreviousData(data):
 
 try:
     verifyFolder(outputFolder)
-    
+
     arcpy.AddMessage('Creating Feature Layer')
     arcpy.MakeFeatureLayer_management(reportsFC, reports, defQuery)
-    
+
     if arcpy.GetCount_management(area).getOutput(0) != '0':
         arcpy.AddMessage('Selecting by Location')
         arcpy.SelectLayerByLocation_management(reports, 'INTERSECT', area)
-    
+
     # export to file type
     if fileType == 'shape':
         removePreviousData(outputFolder + '\\' + outShapeFileName)
-        
+
         arcpy.AddMessage('Exporting to shapefile')
         if defQuery != '1 = 1':
             arcpy.FeatureClassToFeatureClass_conversion(reports, outputFolder, outShapeFileName)
         else:
             arcpy.FeatureClassToShapefile_conversion(reports, outputFolder)
-        
+
         zipPath = outputFolder + r'\roadkill_reports.zip'
         zip = ZipFile(zipPath, 'w', ZIP_DEFLATED)
-        
+
         for fname in listdir(outputFolder):
             if fname.find('.gdb') == -1 and fname.find('.zip') == -1 and fname.find('messages.xml'):
                 zip.write(outputFolder + '\\' + fname, fname)
         zip.close()
-        
+
         outFile = zipPath
     else:
         removePreviousData(outputFolder + '\\' + outDBFName)
-        
+
         arcpy.AddMessage('Exporting to dbf')
         arcpy.TableToTable_conversion(reports, outputFolder, outDBFName)
-        
+
         outFile = outputFolder + '\\' + outDBFName
-    
+
     print outFile
     arcpy.SetParameterAsText(3, outFile)
 except:
